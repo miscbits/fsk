cb892658410fba918630482d7315b5f221a5a717
vagrant <vagrant@homestead>
2016-08-25 05:20:25 +0000

    fixed register route

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,4 +1,4 @@
 <h1>Full Spectrum Kai invitation email</h1>
 <p>below you will find your unique invite link to the fsk site. your login will be used for testing for now but new functionality will be available in time. check back often or contact Wilhem @ willow6786@gmail.com for more details!</p>
 
-<a href='fsk.wilhemarthur.com/{{$token->token}}'>fsk.wilhemarthur.com/{{$token->token}}</a>
+<a href='fsk.wilhemarthur.com/register/{{$token->token}}'>fsk.wilhemarthur.com/register/{{$token->token}}</a>
