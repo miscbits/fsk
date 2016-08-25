ab91130bf5817765fc23b3b95d94bb21e0b31ac6
miscbits <willow6786@gmail.com>
2016-08-25 01:29:51 -0400

    edited reg routes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout');
 // Registration Routes...
 Route::get('register/{token}', 'Auth\RegisterController@showRegistrationForm');
-Route::post('register/{token}', 'Auth\RegisterController@register');
+Route::post('register', 'Auth\RegisterController@register');
 // Password Reset Routes...
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
