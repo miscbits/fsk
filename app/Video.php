6af0b9a5c961b488532975e4d31b6aa2abb4d756
miscbits <willow6786@gmail.com>
2016-08-30 22:09:00 -0400

    removed filler in video controller

--- app/Video.php
+++ app/Video.php
@@ -6,24 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'Video';
-
-    /**
-    * The database primary key value.
-    *
-    * @var string
-    */
-    protected $primaryKey = 'id';
-
-    /**
-     * Attributes that should be mass-assignable.
-     *
-     * @var array
-     */
     protected $fillable = ['title', 'link', 'description'];
 }
